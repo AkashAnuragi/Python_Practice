874e51bcc193ccd5409ef7f634608ee4fec1554f
Akash Anuragi <akashanuragi543@gmail.com>
2026-03-24 22:40:11 +0530

    Mini-Project TIC TAC TOE

--- Day21_mini_project.py
+++ Day21_mini_project.py
@@ -4,7 +4,10 @@ TIC TAC TOE
 """
 
 li = [1,2,3,4,5,6,7,8,9]
+wins = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
 player = "X"
+flag =0
+selected = []
 while True:
     print("\n\t\t  TIC_TAC_TOE")
     print(f"\n\t\t   {li[0]} | {li[1]} | {li[2]} ")
@@ -13,11 +16,35 @@ while True:
     print("\t\t  -----------")
     print(f"\t\t   {li[6]} | {li[7]} | {li[8]} ")
 
-    print(f"\n\t\t Player {player} Turn:", end="")
-    ch = int(input())
-    li[ch-1]=player
+    if flag == 1:
+        break
+    if len(selected)==9:
+        print("\n\t\t   MATCH TIE!")
+        break
 
-    if player == "X":
-        player = "O"
+    print(f"\n\t\t Player {player} Turn:", end="")
+    ch = input()
+
+    if not ch.isdigit():
+        print("\n\t\t Please enter a valid number!")
+        continue
+
+    ch = int(ch)
+
+    if ch < 1 or ch>9:
+        print("\n\t\t Enter Valid Number. Only Between 1-9")
+        continue
+    if ch not in selected:
+        selected.append(ch)
+        li[ch-1]=player
+        for a,b,c in wins:
+            if li[a]==li[b] and li[b]==li[c]:
+                print("\n\t\t Player", player, "WIN")
+                flag =1
+
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
     else:
-        player = "X"
+        print("\n\t\t Already Selected Area \n\t\t Try Another Area!")
