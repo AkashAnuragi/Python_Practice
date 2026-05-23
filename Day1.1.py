c6c1282ec059d8e19e4774ca413b8f364a1ddda7
Akash Anuragi <akashanuragi543@gmail.com>
2026-05-23 19:56:46 +0530

    Guess Output

---
+++ Day1.1.py
@@ -0,0 +1,49 @@
+# Assignment-> Guess Output
+
+a = 5 
+b = 10 
+print(a < b < 20)  # 5<10<20->true<true->true
+
+
+
+
+x = True 
+y = False 
+print(x + y * x) # Jaha operators aa jate hai vaha boolean numeric ki tarah kaaam krte hain-> 1+1*1->1
+
+
+print(4 ** 0 ** 2)  # (4^0)^2->0
+
+
+# 4. Find the result of this bitwise operation: 
+
+a = 12   
+b = 5    
+print(a ^ b)    # 9
+
+
+print((3 and 0) or (0 and 3)) # 0
+
+
+print(256 is 256)  #true
+print(257 is 257)  #true
+
+
+a = 7 
+print(~a + 1) #-7
+
+
+a = True 
+b = False 
+print((a or b) and not (a and b))   #True
+
+
+print(10 > 5 == True)  # False
+
+print(2 + 3 * 4 ** 2 // 8 % 3)
+
+
+print(1 << 2 + 1)  # 8
+
+
+
